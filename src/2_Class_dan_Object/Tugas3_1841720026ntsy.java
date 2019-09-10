b929d2817210b1ebcba89ce64d78c1a789f41651
NatasyaMutyara <54783669+NatasyaMutyara@users.noreply.github.com>
2019-09-10 09:39:12 +0700

    Add files via upload

---
+++ src/2_Class_dan_Object/Tugas3_1841720026ntsy.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jobsheet2;
+
+/**
+ *
+ * @author Natasya Mutyara
+ */
+public class Tugas3_1841720026ntsy {
+    double phi = 3.14;
+    double r ;
+    
+    double hitungLuas(){
+        return phi*r*r;
+    }
+    
+    double hitungKeliling(){
+        return 2*phi*r;
+    }    
+}
