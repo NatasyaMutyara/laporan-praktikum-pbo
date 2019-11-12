5f1d8bd6ff2d3f64a6a4130b018b46cf3d2f56b5
Mutyara <natasyamutyara@gmail.com>
2019-11-12 11:55:14 +0700

    commit jobsheet 11

---
+++ src/11_GUI/LayoutGUI1841720026Ntsy.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ * 
+ * @author Natasya Mutyara
+ */
+import javax.swing.JFrame;
+
+public class LayoutGUI1841720026Ntsy {
+        public static void main(String debby[]) {
+        JFrame frame = new Border1841720026Ntsy();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+        JFrame frame2 = new Grid1841720026Ntsy();
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame2.setVisible(true);
+        
+        JFrame frame3 = new Box1841720026Ntsy();
+        frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame3.setVisible(true);
+    }
+    
+}
